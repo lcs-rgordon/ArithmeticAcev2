9c2e949bbcbcb40e9721c2257f702a35cf00927d
Russell Gordon <github@russellgordon.ca>
2022-02-08 09:20:22 -0500

    Got the factors, 5 and 6, arranged vertically.

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("5")
+            Text("6")
+        }
+        .font(.system(size: 72))
     }
 }
 
